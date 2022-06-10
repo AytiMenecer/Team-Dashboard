dfb72b266f455d3c1d74b5058f386d435c061faa
Nail Ismayilov <nailsmylv@gmail.com>
2022-06-10 16:47:11 +0400

    Team Dashboard

---
+++ app.js
@@ -0,0 +1,26 @@
+// const MY_CHART = document.getElementById('myChart')
+
+// const labels = Utils.months({ count: 7 });
+// const data = {
+//     labels: labels,
+//     datasets: [{
+//         label: 'My First Dataset',
+//         data: [65, 59, 80, 81, 56, 55, 40],
+//         fill: false,
+//         borderColor: 'rgb(75, 192, 192)',
+//         tension: 0.1
+//     }]
+// };
+
+// const myChart = new Chart(ctx, {
+//     MY_CHART
+// });
+
+// const Chart = require('chart.js');
+// const myChart = new Chart(ctx, { MY_CHART });
+
+
+// const config = {
+//     type: 'line',
+//     data: data,
+// };
